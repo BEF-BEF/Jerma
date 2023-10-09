e7858ce33e68fae1ac57fc4362c4b1ff5c715957
BEFBEF <ryanperrycarlson@gmail.com>
2023-10-09 03:39:53 +0000

    Transcribed two streams

--- installLargev2Model.py
+++ installLargev2Model.py
@@ -1,5 +1,13 @@
 # this code can be ran to automatically install large-v2 model so we can transcribe with maximum quality
+
 from faster_whisper import WhisperModel
 
+  
+
 model_size = "large-v2"
+
+model = WhisperModel(model_size, device="cuda", compute_type="float16")
+
+model_size = "medium"
+
 model = WhisperModel(model_size, device="cuda", compute_type="float16")
